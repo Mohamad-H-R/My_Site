fb480d7cd32e98198aabbd05427134217b03239f
Mohamad-H-R <rostami.mohamad.h@gmail.com>
2026-09-13 14:31:34 +0330

    Update admin.py
    updating admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title','status','published_date','created_date')
+admin.site.register(Post,PostAdmin)
